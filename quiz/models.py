495fa34e8c98613a1549e3dfa4f9bc0283c9cc1d
Kay Cha <kay@hannal.net>
2016-07-02 11:55:25 +0900

    Quiz 모델과 Question 모델 추가

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    _choices = [
+        ('open', '열림'),
+        ('close', '종료'),
+        ('draft', '작성 중'),
+    ]
+    title = models.CharField(max_length=250)
+    status = models.CharField(max_length=5, choices=_choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz)
+    seq = models.SmallIntegerField()
+    title = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='%Y/%m/%d/')
+    content = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
